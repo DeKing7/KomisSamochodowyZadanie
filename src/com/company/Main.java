3d2a7835e1d82c35ba646b3b466bc8d81ec1521a
Damian Kardela <DamKar@gmail.com>
2019-03-16 13:27:52 +0100

    new menu 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,29 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+    System.out.println("Witaj użytkowniku wybierz jaka czynność chcesz wykonać");
+    System.out.println("Dodanie nowego samochodu wybierz 1");
+    System.out.println("Wyświetl listę samochodów wybierz 2");
+    System.out.println("Usuń samochód z list wybierz 3");
+        Scanner input = new
+                Scanner(System.in);
+        System.out.println("Wpisz polecenie");
+
+        int czynnosc = input.nextInt();
+        if (czynnosc > 3 ) {
+            System.out.println("Poddałeś nieprawidłowe działanie");
+        } else {
+            System.out.println("Wybrałeś " + czynnosc);
+
+        }
+
+
+
     }
+
 }
