08299bc0027033d077069eddf2e630b8726f8376
Damian Kardela <DamKar@gmail.com>
2019-03-16 16:01:30 +0100

    end exercise 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,29 +1,67 @@
 package com.company;
 
+import com.company.cars.Samochod;
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-    System.out.println("Witaj użytkowniku wybierz jaka czynność chcesz wykonać");
-    System.out.println("Dodanie nowego samochodu wybierz 1");
-    System.out.println("Wyświetl listę samochodów wybierz 2");
-    System.out.println("Usuń samochód z list wybierz 3");
-        Scanner input = new
-                Scanner(System.in);
-        System.out.println("Wpisz polecenie");
+        while (true) {
+            System.out.println("Witaj użytkowniku wybierz jaka czynność chcesz wykonać");
+            System.out.println("Dodanie nowego samochodu wybierz 1");
+            System.out.println("Wyświetl listę samochodów wybierz 2");
+            System.out.println("Usuń samochód z list wybierz 3");
+            Scanner input = new
+                    Scanner(System.in);
+            System.out.println("Wpisz polecenie");
 
-        int czynnosc = input.nextInt();
-        if (czynnosc > 3 ) {
-            System.out.println("Poddałeś nieprawidłowe działanie");
-        } else {
-            System.out.println("Wybrałeś " + czynnosc);
+            int czynnosc = input.nextInt();
 
+            switch (czynnosc) {
+                case 1:
+                    System.out.println("Wybrałes dodanie nowego samochodu, wpisz cechy");
+                    Scanner input2 = new
+                            Scanner(System.in);
+                            System.out.println("Wpisz kolor");
+                            String getKolor = input2.nextLine();
+                            System.out.println("Wpisz marke");
+                            String getMarka = input2.nextLine();
+                            System.out.println("Wpisz cene");
+                            long getCena = input2.nextLong();
+                            System.out.println("Wpisz przebieg");
+                            long getPrzebieg = input2.nextLong();
+                            System.out.println("Wpisz rocznik");
+                            int getRocznik = input2.nextInt();
+                    Samochod car = new Samochod();
+                    car.setCena(getCena);
+                    car.setKolor(getKolor);
+                    car.setMarka(getMarka);
+                    car.setPrzebieg(getPrzebieg);
+                    car.setRocznik(getRocznik);
+                    break;
+                case 2:
+                    System.out.println("Wybrałeś listę samochodów");
+                    break;
+                case 3:
+                    System.out.println("Wybrałeś usunięcie samochodu");
+                    break;
+                    default:
+                        System.out.println("Wybrałeś złe polecenie");
+                        break;
+            }
         }
 
-
-
     }
-
 }
+
+
+
+
+
+
+
+
+
+
